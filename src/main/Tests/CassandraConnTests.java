b6fca70183f46d9e9f0ad71361474264eb835788
Sandeep Talware <34301677+sandeep-talware@users.noreply.github.com>
2018-02-01 19:16:57 +0530

    Update CassandraConnTests.java

--- src/main/Tests/CassandraConnTests.java
+++ src/main/Tests/CassandraConnTests.java
@@ -7,9 +7,9 @@ public class CassandraConnTests {
 	public static void main(String args []) {	
 		System.out.println("Start Program");
 		CassandraConnection client=new CassandraConnection();
-		System.out.println("Attempting connection with 192.168.24.45");
-		client.connection("192.168.24.45", 9042,null);
-		System.out.println("Successful connection with 192.168.24.45");
+		//System.out.println("Attempting connection with Specified IP and Port");
+		client.connection(IP , Port,null);
+		System.out.println("Successful connection with Specified IP and Port");
 		System.out.println("Closing connection");
 		client.close();
 	}
